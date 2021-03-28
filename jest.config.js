6e0a6a1747a4b9e53d1909c065417ba8a411ea12
igorheije <heije.tomori2014@outlook.com>
2021-03-28 15:57:30 -0300

    Create component Logo

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,17 @@ module.exports = {
     testEnvironment: 'jsdom',
     testPathIgnorePattern: ['/node_modules/', '/.next/'],
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+    collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+    modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
 }
